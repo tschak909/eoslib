c35b15cf19307732888265d3b90a896904d34615
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-08-15 13:15:47 -0500

    make sure test program properly initializes VDP

--- tests/console-output/scratchpad.c
+++ tests/console-output/scratchpad.c
@@ -7,8 +7,10 @@
 void main(void)
 {
   int i=0;
-  eos_write_vdp_register(0,1); // Graphics I
-  eos_write_vdp_register(1,0xe0);
+  eos_set_vdp_ports();
+  eos_write_vdp_register(0,1);
+  eos_write_vdp_register(1,0xE0);
+  eos_write_vdp_register(7,0xF5);
   eos_set_vram_table_address(0,0x1b00);
   eos_set_vram_table_address(1,0x3800);
   eos_set_vram_table_address(2,0x1800);
